c531c707271d6aaa807a2e7f3905bf17f05f9929
4k.programmer <4k.programmer@gmail.com>
2023-03-09 22:37:46 +0330

    log received data and show on endpoint

--- main.go
+++ main.go
@@ -2,40 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"net/http"
 	"os"
-	"time"
+)
+
+const (
+	HOST      = "94.101.186.207"
+	TCP_PORT  = "8080"
+	HTTP_PORT = "8081"
+	TYPE      = "tcp"
 )
 
 func main() {
-	// Create a new file for logging
-	logFile, err := os.OpenFile("/var/log/tcpserver.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// Create the log file
+	logFile, err := os.OpenFile("messages.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Println("Error opening log file:", err.Error())
-		return
+		log.Fatal("Error opening log file:", err)
 	}
 	defer logFile.Close()
 
-	const (
-		HOST = "94.101.186.207"
-		PORT = "8080"
-		TYPE = "tcp"
-	)
+	// Start a HTTP server on port 8080
+	go func() {
+		http.HandleFunc("/log", handleLog)
+		http.ListenAndServe(HOST+":"+HTTP_PORT, nil)
+	}()
 
-	// Listen for incoming connections on port 8080
-	listener, err := net.Listen(TYPE, HOST+":"+PORT)
+	// Listen for incoming connections on port 8081
+	listener, err := net.Listen("tcp", HOST+":"+TCP_PORT)
 	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		return
+		log.Fatal("Error listening:", err)
 	}
 	defer listener.Close()
-	fmt.Println("Listening on :8080")
+	fmt.Println("Listening on :8081")
 
 	for {
 		// Wait for a connection
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Error accepting connection:", err.Error())
+			log.Println("Error accepting connection:", err)
 			continue
 		}
 
@@ -50,29 +57,50 @@ func handleConnection(conn net.Conn, logFile *os.File) {
 	for {
 		// Read data from the connection
 		buffer := make([]byte, 1024)
-		_, err := conn.Read(buffer)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("Error reading:", err.Error())
+			log.Println("Error reading:", err)
 			return
 		}
 
-		// Log the received data to the file
-		timestamp := time.Now().Format("2006-01-02 15:04:05")
-		logMsg := fmt.Sprintf("%s - Received data: %s\n", timestamp, string(buffer))
-		_, err = logFile.WriteString(logMsg)
-		if err != nil {
-			fmt.Println("Error writing to log file:", err.Error())
-			return
-		}
+		// Print the received data to the console
+		message := string(buffer[:n])
+		fmt.Println("Received data:", message)
+
+		// Write the received message to the log file
+		logFile.Println(message)
 
 		// Send a response back to the client
 		response := "Hello, client!"
 		_, err = conn.Write([]byte(response))
 		if err != nil {
-			fmt.Println("Error writing:", err.Error())
+			log.Println("Error writing:", err)
 			return
 		}
 
-		fmt.Println("Response sent")
+		// Wait for a second before sending the next response
+		//time.Sleep(time.Second)
+	}
+}
+
+func handleLog(w http.ResponseWriter, r *http.Request) {
+	// Open the log file and write its contents to the HTTP response
+	logFile, err := os.Open("messages.log")
+	if err != nil {
+		http.Error(w, "Error opening log file", http.StatusInternalServerError)
+		return
+	}
+	defer logFile.Close()
+
+	_, err = logFile.Seek(0, 0)
+	if err != nil {
+		http.Error(w, "Error seeking log file", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = io.Copy(w, logFile)
+	if err != nil {
+		http.Error(w, "Error writing log to HTTP response", http.StatusInternalServerError)
+		return
 	}
 }
